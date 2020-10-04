21eacb942f433734ec23a3466c092b4cd1f31f69
taejinseok <stj930@gmail.com>
2020-10-05 00:01:24 +0900

    feat: cron 표현식을 통한 알람봇 스케쥴러 추가

---
+++ src/main/java/kr/taggle/alarmbot/config/SchedulerConfig.java
@@ -0,0 +1,9 @@
+package kr.taggle.alarmbot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@Configuration
+public class SchedulerConfig {
+}
