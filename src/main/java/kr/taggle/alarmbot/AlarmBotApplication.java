72a01b46394087ab53f6f75158722ff360952786
taejinseok <stj930@gmail.com>
2020-10-04 22:50:34 +0900

    init: setup project

---
+++ src/main/java/kr/taggle/alarmbot/AlarmBotApplication.java
@@ -0,0 +1,13 @@
+package kr.taggle.alarmbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlarmBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlarmBotApplication.class, args);
+    }
+
+}
